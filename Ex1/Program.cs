bfb8f2c8eaaad358b644d07a923ef45a67d529a2
EricAOlivei <oliveira_eric@educadventista.org>
2022-02-25 10:48:44 -0300

    Add project files.

---
+++ Ex1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Ex1.Entities;
+using System.Globalization;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of empleyees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i}");
+
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourceEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine();
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("PAYMANTS: ");
+
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+
+            }
+        }
+    }
+}
